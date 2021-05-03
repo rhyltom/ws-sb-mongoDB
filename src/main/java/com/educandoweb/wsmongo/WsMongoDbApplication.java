4a1283d2c9950f2edbb805c810a14a61fb8c62f9
Emanuel <dendiiiman@gmail.com>
2021-05-03 10:27:40 +0100

    primeiro commit

---
+++ src/main/java/com/educandoweb/wsmongo/WsMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.wsmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/wsmongo/WsMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.wsmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
