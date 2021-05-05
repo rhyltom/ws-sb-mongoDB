acbf2b55f087a7eb4398733935d2326136329d4f
Emanuel <dendiiiman@gmail.com>
2021-05-05 10:08:42 +0100

    consulta simples com query methods

--- src/main/java/com/educandoweb/wsmongo/resources/PostResource.java
+++ src/main/java/com/educandoweb/wsmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.wsmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.wsmongo.domain.Post;
+import com.educandoweb.wsmongo.resources.util.URL;
 import com.educandoweb.wsmongo.services.PostService;
 
 @RestController
@@ -16,11 +20,19 @@ public class PostResource {
 
 	@Autowired
 	private PostService service;
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 
 }
--- src/main/java/com/educandoweb/wsmongo/services/PostService.java
+++ src/main/java/com/educandoweb/wsmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.educandoweb.wsmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.wsmongo.domain.Post;
-import com.educandoweb.wsmongo.domain.User;
 import com.educandoweb.wsmongo.repository.PostRepository;
 import com.educandoweb.wsmongo.services.exceptions.ObjNotFoundException;
 
@@ -21,4 +21,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjNotFoundException("obj nao encontrado"));
 	}
 
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
+	
 }
