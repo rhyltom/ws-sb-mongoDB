df15cc27ca6c28bb4586ef88867cc93dfb828b02
Emanuel <dendiiiman@gmail.com>
2021-05-05 09:47:15 +0100

    Acrescentar comentarios aos Posts

--- src/main/java/com/educandoweb/wsmongo/config/Instantiation.java
+++ src/main/java/com/educandoweb/wsmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.educandoweb.wsmongo.domain.Post;
 import com.educandoweb.wsmongo.domain.User;
 import com.educandoweb.wsmongo.dto.AuthorDTO;
+import com.educandoweb.wsmongo.dto.CommentDTO;
 import com.educandoweb.wsmongo.repository.PostRepository;
 import com.educandoweb.wsmongo.repository.UserRepository;
 
@@ -35,10 +36,20 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
+		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar para S. Paulo", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "bom dia", "acordei feliz", new AuthorDTO(maria));		
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "bom dia", "acordei feliz", new AuthorDTO(maria));	
+		
+		CommentDTO c1 = new CommentDTO("boa viagem", sdf.parse("21/03/2008"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("aproveite", sdf.parse("22/03/2008"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("otimo dia", sdf.parse("23/03/2008"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
