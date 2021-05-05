341b410d880960402890b75f3a2a995e23f92ad9
Emanuel <dendiiiman@gmail.com>
2021-05-05 09:36:39 +0100

    obtentdo POST por ID

---
+++ src/main/java/com/educandoweb/wsmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.educandoweb.wsmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.wsmongo.domain.Post;
+import com.educandoweb.wsmongo.domain.User;
+import com.educandoweb.wsmongo.repository.PostRepository;
+import com.educandoweb.wsmongo.services.exceptions.ObjNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjNotFoundException("obj nao encontrado"));
+	}
+
+}
